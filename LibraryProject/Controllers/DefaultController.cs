769ba36bbfafaecd291adcdcdb26f8bafaf64f43
rawda <rawda95.r@gmail.com>
2019-03-14 23:12:30 +0200

    design change

---
+++ LibraryProject/Controllers/DefaultController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+
+namespace LibraryProject.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+    }
+}
